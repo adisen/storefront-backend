0e7d9b617b958ec51fb2cd98de4514886293349d
Israel <israeleadetunji@gmail.com>
2023-02-25 21:33:52 +0100

    update orders migrations

--- migrations/sqls/20230118121853-orders-up.sql
+++ migrations/sqls/20230118121853-orders-up.sql
@@ -2,4 +2,4 @@
 DROP TYPE IF EXISTS orderStatus;
 CREATE TYPE orderStatus AS ENUM ('active', 'complete');
 
-CREATE TABLE orders(id SERIAL PRIMARY KEY, quantity INTEGER, status orderStatus, product_id bigint REFERENCES products(id), user_id bigint REFERENCES users(id));
+CREATE TABLE orders(id SERIAL PRIMARY KEY, status orderStatus, user_id bigint REFERENCES users(id));
